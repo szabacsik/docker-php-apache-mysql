29c8e887d73d58a2ee9a296f2b40fd1b009a7970
András Szabácsik <andras@szabacsik.hu>
2020-11-26 18:31:40 +0100

    Use rsync to sync new or modified files in projects provision script

--- provisioning/shell/projects-start.sh
+++ provisioning/shell/projects-start.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 rm -f /etc/apache2/sites-enabled/000-default.conf
-mv /tmp/projects/apache.conf /etc/apache2/sites-enabled/apache.conf
-rm -rf /var/www/html/demo
-rm -rf /var/www/html/info
-mv /tmp/projects/* /var/www/html
+mv -f /tmp/projects/apache.conf /etc/apache2/sites-enabled/apache.conf
+rsync /tmp/projects/ /var/www/html \
+  --recursive \
+  --checksum \
+  --ignore-times \
+  --delete \
+  --prune-empty-dirs \
+  --verbose \
+  --human-readable \
+  --remove-source-files \
+  -og \
+  --chown=vagrant:vagrant \
+  --perms \
+  --chmod=Dug=rwx,Fug=rw,Do=rx,Fo=r
 service apache2 restart
