eb17e6b936785ff91807586b7e27a99138e6389f
András Szabácsik <andras@szabacsik.hu>
2021-02-01 17:33:55 +0100

    Rename 'projects' folder to 'www'

--- provisioning/shell/www-deploy.sh
+++ provisioning/shell/www-deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 rm -f /etc/apache2/sites-enabled/000-default.conf
-mv -f /tmp/projects/apache.conf /etc/apache2/sites-enabled/apache.conf
-rsync /tmp/projects/ /srv/www \
+mv -f /tmp/www/apache.conf /etc/apache2/sites-enabled/apache.conf
+rsync /tmp/www/ /srv/www \
   --recursive \
   --checksum \
   --ignore-times \
